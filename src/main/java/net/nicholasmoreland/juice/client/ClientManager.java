bbbd05fc75e314630a5111b97580d450dc7d7030
nicholasmoreland <nmoreland18@outlook.com>
2021-06-14 16:10:15 -0500

    Added event listening for client manager

--- src/main/java/net/nicholasmoreland/juice/client/ClientManager.java
+++ src/main/java/net/nicholasmoreland/juice/client/ClientManager.java
@@ -1,14 +1,46 @@
 package net.nicholasmoreland.juice.client;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
-public class ClientManager {
+import java.util.HashMap;
+import java.util.UUID;
 
-    private final List<Client> clientList = new ArrayList<>();
+public class ClientManager implements Listener {
 
-    public List<Client> getClientList() {
-        return clientList;
+    private final HashMap<UUID, Client> clientMap = new HashMap<>();
+
+    public HashMap<UUID, Client> getClientMap() {
+        return clientMap;
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    private void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        UUID uuid = player.getUniqueId();
+        String name = player.getName();
+        Client client;
+
+        if (name.equals("FaultyRam")) {
+            client = new Client(name, ClientRank.DEV);
+        } else {
+            client = new Client(name);
+        }
+        clientMap.put(uuid, client);
+        System.out.printf("%s with rank of %s has been added to client map.", name, client.getRank().toString());
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    private void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        UUID uuid = player.getUniqueId();
+
+        clientMap.remove(uuid);
+        System.out.printf("%s has been removed from client map.", player.getName());
     }
 
 }
